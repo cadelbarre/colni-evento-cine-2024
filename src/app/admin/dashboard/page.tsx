8d25d77557e4f5b17977e2df14cd67748a96b3d3
Carlos Delbarre <cadelbarre@gmail.com>
2025-10-14 19:20:32 -0500

    refactor: standardize code formatting and improve readability in Dashboard component

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,60 +1,70 @@
-'use client'
-import { useEffect } from 'react'
-import useSWR, { Fetcher } from 'swr'
-import { useRouter } from 'next/navigation'
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+import type { Fetcher } from "swr";
 
-import { useAuthStore } from './store/auth-store'
-import { UserData } from '@/types/user'
+import { useAuthStore } from "./store/auth-store";
+import type { UserData } from "@/types/user";
 
-import Navbar from './components/navbar'
-import Table from './components/table'
-import SkeletonTable from './components/skeleton-table'
+import Navbar from "./components/navbar";
+import Table from "./components/table";
+import SkeletonTable from "./components/skeleton-table";
 
-const fetcher: Fetcher<UserData[], string> = async (...args) => await fetch(...args).then(async res => await res.json())
+const fetcher: Fetcher<UserData[], string> = async (...args) =>
+  await fetch(...args).then(async (res) => await res.json());
 
-export default function Dashboard (): JSX.Element {
-  const { isAuth } = useAuthStore()
-  const { data, isLoading, mutate } = useSWR('/api/users', fetcher)
-  const router = useRouter()
+export default function Dashboard(): JSX.Element {
+  const { isAuth } = useAuthStore();
+  const { data, isLoading, mutate } = useSWR("/api/users", fetcher);
+  const router = useRouter();
 
   useEffect(() => {
     if (!isAuth) {
-      router.push('/admin')
+      router.push("/admin");
     }
-  }, [])
+  }, []);
 
   if (!isAuth) {
     return (
       <main>
-        <div className='max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8'>
-          <div className='max-w-lg mx-auto text-center'>
-            <h3 className='text-gray-800 text-4xl font-semibold sm:text-5xl'>
+        <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+          <div className="max-w-lg mx-auto text-center">
+            <h3 className="text-gray-800 text-4xl font-semibold sm:text-5xl">
               Validando información
             </h3>
-            <p className='text-gray-600 mt-3 mb-8'>
+            <p className="text-gray-600 mt-3 mb-8">
               Estamos validando las credenciales de tu cuenta.
             </p>
-            <div className='grid place-content-center'>
-              <svg className='spinner' viewBox='0 0 50 50'>
-                <circle className='path' cx='25' cy='25' r='20' fill='none' strokeWidth='5' />
+            <div className="grid place-content-center">
+              <svg className="spinner" viewBox="0 0 50 50">
+                <title>Loading</title>
+                <circle
+                  className="path"
+                  cx="25"
+                  cy="25"
+                  r="20"
+                  fill="none"
+                  strokeWidth="5"
+                />
               </svg>
             </div>
           </div>
         </div>
       </main>
-    )
+    );
   }
 
   return (
-    <main className='flex flex-col w-full'>
-      <Navbar className='col-span-2' />
-      <div className='p-4 md:p-8 col-span-10 overflow-y-auto h-screen'>
-        {
-            isLoading
-              ? <SkeletonTable />
-              : <Table data={data ?? []} mutate={mutate} />
-        }
+    <main className="flex flex-col w-full max-w-7xl mx-auto">
+      <Navbar className="col-span-2" />
+      <div className="p-4 md:p-8 col-span-10 h-screen">
+        {isLoading ? (
+          <SkeletonTable />
+        ) : (
+          <Table data={data ?? []} mutate={mutate} />
+        )}
       </div>
     </main>
-  )
+  );
 }

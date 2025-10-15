7600c3c128041328ea9070b1f7313b736880110e
Carlos Delbarre <cadelbarre@gmail.com>
2025-10-14 19:20:37 -0500

    refactor: improve code formatting and structure in Actions component

--- src/app/admin/dashboard/components/actions.tsx
+++ src/app/admin/dashboard/components/actions.tsx
@@ -1,95 +1,126 @@
-'use client'
-import { toast } from 'sonner'
-import { KeyedMutator } from 'swr'
-import swal from 'sweetalert'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+"use client";
+import { toast } from "sonner";
+import swal from "sweetalert";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import {
   ChevronDownIcon,
   PencilIcon,
-  TrashIcon
-} from '@heroicons/react/16/solid'
+  TrashIcon,
+} from "@heroicons/react/16/solid";
 
-import { UserData } from '@/types/user'
-import { sendEmail } from '@/app/components/form'
+import { sendEmail } from "@/app/components/form";
 
-export default function Actions ({ user, mutate }: { user: UserData, mutate: KeyedMutator<UserData[]> }): JSX.Element {
-  const showWarning = async (action: 'edit' | 'delete'): Promise<void> => {
+import type { UserData } from "@/types/user";
+import type { KeyedMutator } from "swr";
+
+export default function Actions({
+  user,
+  mutate,
+}: {
+  user: UserData;
+  mutate: KeyedMutator<UserData[]>;
+}): JSX.Element {
+  const showWarning = async (action: "edit" | "delete"): Promise<void> => {
     await swal({
-      title: '¿Seguro/a que deseas hacer esta acción?',
-      text: action === 'edit' ? 'El usuario se actualizará' : 'El usuario se eliminará permanentemente',
-      icon: 'warning',
-      buttons: ['Cancelar', action === 'edit' ? 'Actualizar' : 'Eliminar'],
-      dangerMode: action === 'delete'
-
+      title: "¿Seguro/a que deseas hacer esta acción?",
+      text:
+        action === "edit"
+          ? "El usuario se actualizará"
+          : "El usuario se eliminará permanentemente",
+      icon: "warning",
+      buttons: ["Cancelar", action === "edit" ? "Actualizar" : "Eliminar"],
+      dangerMode: action === "delete",
     }).then(async (willDelete: boolean) => {
       if (willDelete) {
-        await manageUser(action)
+        await manageUser(action);
         await swal(
-          action === 'edit' ? 'Actualizado!' : 'Eliminado!',
-          action === 'edit' ? 'El usuario ha sido actualizado correctamente' : 'El ususario ha sido eliminado correctamente',
-          'success')
+          action === "edit" ? "Actualizado!" : "Eliminado!",
+          action === "edit"
+            ? "El usuario ha sido actualizado correctamente"
+            : "El ususario ha sido eliminado correctamente",
+          "success",
+        );
       }
-    })
-  }
+    });
+  };
 
-  const manageUser = async (action: 'edit' | 'delete'): Promise<void> => {
+  const manageUser = async (action: "edit" | "delete"): Promise<void> => {
     const data = {
       ...user,
-      ...(action === 'edit' && { payment: !user.payment })
-    }
+      ...(action === "edit" && { payment: !user.payment }),
+    };
 
-    const body = await fetch('/api/users', {
-      method: action === 'edit' ? 'PUT' : 'DELETE',
+    const body = await fetch("/api/users", {
+      method: action === "edit" ? "PUT" : "DELETE",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    })
+      body: JSON.stringify(data),
+    });
 
-    const json = await body.json()
+    const json = await body.json();
 
     if (body.ok) {
-      toast.success(action === 'edit' ? 'Usuario actualizado exitosamente' : 'Usuario eliminado exitosamente')
-      await mutate()
+      toast.success(
+        action === "edit"
+          ? "Usuario actualizado exitosamente"
+          : "Usuario eliminado exitosamente",
+      );
+      await mutate();
     } else {
-      toast.error(json.message)
-      toast.error(action === 'edit' ? 'Error al actualizar el usuario' : 'Error al eliminar el usuario')
+      toast.error(json.message);
+      toast.error(
+        action === "edit"
+          ? "Error al actualizar el usuario"
+          : "Error al eliminar el usuario",
+      );
     }
-  }
+  };
 
   return (
     <Menu>
-      <MenuButton className='inline-flex items-center gap-2 rounded-md bg-blue-50 py-1.5 px-3 text-sm/6 font-semibold text-blue-600 shadow shadow-black/10 focus:outline-none data-[hover]:bg-blue-700 data-[open]:bg-blue-700  data-[open]:text-white data-[focus]:outline-1 data-[focus]:outline-white hover:text-white group'>
+      <MenuButton className="inline-flex items-center gap-2 rounded-md bg-blue-50 py-1.5 px-3 text-sm/6 font-semibold text-blue-600 shadow shadow-black/10 focus:outline-none data-[hover]:bg-blue-700 data-[open]:bg-blue-700  data-[open]:text-white data-[focus]:outline-1 data-[focus]:outline-white hover:text-white group">
         Acciones
-        <ChevronDownIcon className='size-4 fill-gray-600 group-hover:fill-white' />
+        <ChevronDownIcon className="size-4 fill-gray-600 group-hover:fill-white" />
       </MenuButton>
 
       <MenuItems
         transition
-        anchor='bottom end'
-        className='w-52 origin-top-right rounded-xl border border-gray-300 bg-white p-1 text-sm/6 text-gray-600 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0'
+        anchor="bottom end"
+        className="w-52 origin-top-right rounded-xl border border-gray-300 bg-white p-1 text-sm/6 text-gray-600 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
       >
         <MenuItem>
-          <button onClick={async () => await sendEmail(user)} className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-blue-100'>
-            <PencilIcon className='size-4 fill-gray-600' />
+          <button
+            type="button"
+            onClick={async () => await sendEmail(user)}
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-blue-100"
+          >
+            <PencilIcon className="size-4 fill-gray-600" />
             Enviar correo
           </button>
         </MenuItem>
         <MenuItem>
-          <button onClick={async () => await showWarning('edit')} className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-blue-100'>
-            <PencilIcon className='size-4 fill-gray-600' />
+          <button
+            type="button"
+            onClick={async () => await showWarning("edit")}
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-blue-100"
+          >
+            <PencilIcon className="size-4 fill-gray-600" />
             Pagó
           </button>
         </MenuItem>
-        <div className='my-1 h-px bg-gray-200' />
+        <div className="my-1 h-px bg-gray-200" />
         <MenuItem>
-          <button onClick={async () => await showWarning('delete')} className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-rose-500 hover:text-white group'>
-            <TrashIcon className='size-4 fill-gray-600 group-hover:fill-white' />
+          <button
+            type="button"
+            onClick={async () => await showWarning("delete")}
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-rose-500 hover:text-white group"
+          >
+            <TrashIcon className="size-4 fill-gray-600 group-hover:fill-white" />
             Eliminar
           </button>
         </MenuItem>
       </MenuItems>
     </Menu>
-
-  )
+  );
 }

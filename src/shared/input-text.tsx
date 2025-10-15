7987154f7ff230697a6ee2b34d6a002bd92ea574
Carlos Delbarre <cadelbarre@gmail.com>
2025-10-14 19:21:30 -0500

    refactor: improve code formatting and enhance InputText component styling

--- src/shared/input-text.tsx
+++ src/shared/input-text.tsx
@@ -1,44 +1,46 @@
-'use client'
-import { FieldValuesTypes } from '@/app/model'
-import { InputHTMLAttributes } from 'react'
-import { Control, Controller, FieldError } from 'react-hook-form'
+"use client";
+import { FieldValuesTypes } from "@/app/model";
+import { InputHTMLAttributes } from "react";
+import { Control, Controller, FieldError } from "react-hook-form";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  name: keyof FieldValuesTypes
-  label: string
-  control: Control<FieldValuesTypes>
-  error?: FieldError
+  name: keyof FieldValuesTypes;
+  label: string;
+  control: Control<FieldValuesTypes>;
+  error?: FieldError;
 }
 
-export default function InputText ({
+export default function InputText({
   label,
   name,
   error,
   control,
-  type = 'text',
+  type = "text",
   required = false,
   ...attr
 }: Props): JSX.Element {
   return (
     <div>
-      <label className='text-gray-700'>
-        {label} {required && <span className='text-red-500'>*</span>}
+      <label className="text-gray-100">
+        {label} {required && <span className="text-red-500">*</span>}
         <Controller
           name={name}
           control={control}
-          render={({ field }) => <input
-            type={type}
-            {...field}
-            placeholder=''
-            required={required}
-            {...attr}
-            className={`w-full mt-2 px-3 py-2 text-gray-700  border focus:border-indigo-600 shadow-sm rounded-lg bg-gray-50/30 disabled:bg-gray-100 disabled:cursor-wait ${error != null ? 'border-rose-400' : ''}`}
-                                 />}
+          render={({ field }) => (
+            <input
+              type={type}
+              {...field}
+              placeholder=""
+              required={required}
+              {...attr}
+              className={`w-full mt-2 px-3 py-2 text-gray-700  border focus:border-indigo-600 shadow-sm rounded-lg bg-white disabled:bg-gray-100 disabled:cursor-wait ${error != null ? "border-rose-400" : ""}`}
+            />
+          )}
         />
       </label>
-      {
-        (error != null) && <p className='pt-2 text-sm text-rose-400 italic'>{error.message}</p>
-      }
+      {error != null && (
+        <p className="pt-2 text-sm text-rose-400 italic">{error.message}</p>
+      )}
     </div>
-  )
+  );
 }

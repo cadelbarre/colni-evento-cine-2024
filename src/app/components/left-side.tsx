d1e36c650e056fb19d66f3dbacde9bdb6f012334
Carlos Delbarre <cadelbarre@gmail.com>
2025-10-14 19:21:26 -0500

    refactor: update LeftSide component structure and improve background styling

--- src/app/components/left-side.tsx
+++ src/app/components/left-side.tsx
@@ -1,8 +1,22 @@
-
-export default function LeftSide (): JSX.Element {
+// bg-[url("../assets/cine-evento-2025.webp")
+export default function LeftSide(): JSX.Element {
   return (
-    <>
-      <div className='relative flex-1 hidden items-center justify-center h-screen bg-[url("../assets/cine-evento.webp")] bg-no-repeat bg-bottom bg-cover lg:flex' />
-    </>
-  )
+    <div
+      className="relative flex-1 hidden items-center justify-center h-screen lg:flex"
+      style={{
+        background:
+          "linear-gradient(360deg,rgba(51, 51, 51, 1) 0%, rgba(0, 0, 0, 1) 100%)",
+      }}
+    >
+      <img
+        aria-label="flyer de desafio neurovascular"
+        className="size-full object-contain"
+        src="/cine-evento-2025.webp"
+        style={{
+          background:
+            "linear-gradient(360deg,rgba(51, 51, 51, 1) 0%, rgba(0, 0, 0, 1) 100%)",
+        }}
+      />
+    </div>
+  );
 }

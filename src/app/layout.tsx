f88156f05b3c5fbf8554014e9b73462ff7f8c8a6
Carlos Delbarre <cadelbarre@gmail.com>
2025-10-14 19:20:05 -0500

    feat: implement internationalization support with Spanish and English translations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,41 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
-import { Toaster } from 'sonner'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "sonner";
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900'
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900'
-})
+import "@fontsource/cormorant-garamond/700.css";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+//  <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
 
 export const metadata: Metadata = {
-  title: 'Formulario de Incripción - Colni Cine 2024',
-  description: '¡Inscríbete y forma parte de la magia de &quot;Cine el Origen&quot;!',
+  title: "Formulario de Incripción - Desafio Neurovascular 2025",
+  description:
+    "¡Inscríbete y forma parte de la magia de &quot;Cine el Origen&quot;!",
   icons: {
-    icon: 'logo-colni-icon.webp'
-  }
-}
+    icon: "logo-colni-icon.webp",
+  },
+};
 
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>): JSX.Element {
   return (
-    <html lang='es'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="es">
+      <body className={` antialiased`}>
         {children}
+        <Toaster richColors position="top-center" expand />
       </body>
-      <Toaster richColors position='top-center' expand />
     </html>
-  )
+  );
 }
---
+++ src/utils/i18n.ts
@@ -0,0 +1,49 @@
+// i18n.js
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const esTranslations = {
+  title: "Formulario de Incripción - Colni Cine 2025",
+  subtitle:
+    '¡Inscríbete y forma parte de la magia de "El Desafío Neurovascular"!',
+  fullName: "Nombre Completo",
+  email: "Correo Electrónico",
+  cellphone: "Celular",
+  country: "Pais",
+  specialty: "Especialidad",
+  allergies: "¿Posee alguna alergia?",
+  register: "Reservar Cupo",
+  identification: "Cedula",
+  placeholderAllergies: "Ej. Polen, Penicilina, Camaron, etc.",
+};
+
+const enTranslations = {
+  title: "Registration Form - Colni Cine 2025",
+  subtitle:
+    'Sign Up and Be Part of the Magic of "The Neurovascular Challenge"!',
+  fullName: "Full Name",
+  email: "Email",
+  cellphone: "Mobile phone number",
+  country: "Country",
+  specialty: "Specialty",
+  allergies: "Do you have any allergies?",
+  register: "Register",
+  identification: "ID",
+  placeholderAllergies: "Eg. Penicillin, Polen, shrimp, etc.",
+};
+
+(async () => {
+  await i18n.use(initReactI18next).init({
+    resources: {
+      es: { translation: esTranslations },
+      en: { translation: enTranslations },
+    },
+    lng: "es",
+    fallbackLng: "es",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+})();
+
+export default i18n;
